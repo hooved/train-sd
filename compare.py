8f257f74e49b1df5852a5e1fd35546f2c3e25605
hooved <172129504+hooved@users.noreply.github.com>
2025-07-11 16:33:01 -0400

    confirm latent sampling correctness

--- compare.py
+++ compare.py
@@ -3,6 +3,15 @@ from tinygrad.nn.state import safe_load, load_state_dict, get_state_dict
 from typing import Any
 from collections import namedtuple
 
+# goal: translate mlperf stable_diffusion-v2 ref. implementation to tinygrad
+
+# not shown: generation of safetensors from mlperf reference implementation
+# to get these safetensors, setup the mlperf docker image and config (see README), and export the safetensors where indicated in the commented mlperf code
+
+compare_latent = True
+compare_clip = False
+compare_unet = False
+
 def md(a, b):
   diff = (a - b).abs()
   max_diff = diff.max().item()
@@ -10,46 +19,58 @@ def md(a, b):
   ratio = mean_diff / a.abs().mean().item()
   return mean_diff, ratio, max_diff
 
-# goal: translate mlperf stable_diffusion-v2 ref. implementation to tinygrad
+### sampled latent closeness testing
+if compare_latent:
+  data = safe_load("/home/hooved/train-sd/training/stable_diffusion/datasets/tensors/unet_inputs.safetensors")
+  for k,v in data.items():
+    data[k] = v.to("NV").realize()
+  SCALE_FACTOR = 0.18215
+  # sample latent from VAE-generated distribution (NOTE: mlperf ref. starts from mean/logvar loaded from disk, as done here)
 
-# not shown: generation of safetensors from mlperf reference implementation
-# to get these safetensors, setup the mlperf docker image and config (see README), and export the safetensors where indicated in the commented mlperf code
+  mean_logvar = data['batch'].cast(dtypes.half).squeeze(1)
+  mean, logvar = Tensor.chunk(mean_logvar, 2, dim=1)
+  std = Tensor.exp(0.5 * logvar.clamp(-30.0, 20.0))
+  #latent = (mean + std * Tensor.randn(mean.shape)) * SCALE_FACTOR
+  latent = (mean + std * data["latent_randn_sampling"]).cast(dtypes.float16) * SCALE_FACTOR
+  mean_diff, ratio, max_diff = md(data['x'], latent)
+  # (0.0001468658447265625, 0.0001968463678010471, 0.0026092529296875)
 
-# *** clip encoding closeness testing
-from extra.models.clip import FrozenOpenClipEmbedder
-data = safe_load("/home/hooved/train-sd/training/stable_diffusion/datasets/tensors/unet_inputs.safetensors")
-for k,v in data.items():
-  data[k] = v.to("NV").realize()
+### clip encoding closeness testing
+if compare_unet:
+  from extra.models.clip import FrozenOpenClipEmbedder
+  data = safe_load("/home/hooved/train-sd/training/stable_diffusion/datasets/tensors/unet_inputs.safetensors")
+  for k,v in data.items():
+    data[k] = v.to("NV").realize()
 
-with open("/home/hooved/train-sd/training/stable_diffusion/datasets/tensors/cond.txt", encoding="utf-8") as f:
-  prompt = f.read()
+  with open("/home/hooved/train-sd/training/stable_diffusion/datasets/tensors/cond.txt", encoding="utf-8") as f:
+    prompt = f.read()
 
-# For loading mlperf reference weights state_dict
-class ClipContainer:
-  def __init__(self):
-    clip_config = {"dims": 1024, "n_heads": 16, "layers": 24, "return_pooled": False, "ln_penultimate": True}
-    self.cond_stage_model = FrozenOpenClipEmbedder(**clip_config)
+  # For loading mlperf reference weights state_dict
+  class ClipContainer:
+    def __init__(self):
+      clip_config = {"dims": 1024, "n_heads": 16, "layers": 24, "return_pooled": False, "ln_penultimate": True}
+      self.cond_stage_model = FrozenOpenClipEmbedder(**clip_config)
 
-model = ClipContainer()
-mlperf_training_init_model = safe_load("/home/hooved/train-sd/training/stable_diffusion/checkpoints/training_init_model.safetensors")
-load_state_dict(model, mlperf_training_init_model)
-context = model.cond_stage_model([prompt]).realize()
+  model = ClipContainer()
+  mlperf_training_init_model = safe_load("/home/hooved/train-sd/training/stable_diffusion/checkpoints/training_init_model.safetensors")
+  load_state_dict(model, mlperf_training_init_model)
+  context = model.cond_stage_model([prompt]).realize()
 
-"""
-md(data['c'], context)
-(2.1096495856909314e-06, 2.6900317320149505e-06, 2.384185791015625e-05)
+  mean_diff, ratio, max_diff = md(data['c'], context)
 
-to get mean(abs(diff)) < 1e-5, in mlperf ref. implementation you need to do this:
-  - in clip resblocks: self.mlp[1].approximate="tanh", to match tinygrad gelu fastmath
-  - torch.backends.cuda.matmul.allow_tf32 = False
-  - torch.backends.cudnn.allow_tf32 = False
-  - torch.set_float32_matmul_precision("highest")
-"""
+  """
+  md(data['c'], context)
+  (2.1096495856909314e-06, 2.6900317320149505e-06, 2.384185791015625e-05)
 
-if False:
+  to get mean(abs(diff)) < 1e-5, in mlperf ref. implementation you need to do this:
+    - in clip resblocks: self.mlp[1].approximate="tanh", to match tinygrad gelu fastmath
+    - torch.backends.cuda.matmul.allow_tf32 = False
+    - torch.backends.cudnn.allow_tf32 = False
+    - torch.set_float32_matmul_precision("highest")
+  """
 
-  ### unet forward pass (during training), closeness testing
-
+### unet forward pass (during training), closeness testing
+if compare_unet:
   from extra.models.unet import UNetModel
 
   unet_params: dict[str,Any] = {
@@ -82,6 +103,7 @@ if False:
 
   out = model(unet_io['x'], unet_io['timesteps'], unet_io['context']).realize()
 
-  diff, ratio = md(unet_io['out'], out)
+  #diff, ratio = md(unet_io['out'], out)
+  mean_diff, ratio, max_diff = md(data['out'], out)
 
   end = 1
