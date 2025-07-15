cb13a529c57bf73d3da70a58c51fd8e99af62af6
hooved <172129504+hooved@users.noreply.github.com>
2025-07-15 16:10:34 -0400

    compare ref/tinygrad over 10 training steps

--- compare.py
+++ compare.py
@@ -3,16 +3,19 @@ from tinygrad.nn.state import safe_load, load_state_dict, get_state_dict
 from typing import Any
 from collections import namedtuple
 from examples.stable_diffusion import get_alphas_cumprod
+from pathlib import Path
+BASEDIR = Path("/home/hooved/train-sd/training/stable_diffusion")
 
 # goal: translate mlperf stable_diffusion-v2 ref. implementation to tinygrad
 
 # not shown: generation of safetensors from mlperf reference implementation
 # to get these safetensors, setup the mlperf docker image and config (see README), and export the safetensors where indicated in the commented mlperf code
 
+compare_end_to_end = True
 compare_latent = False
 compare_clip = False
 compare_unet = False
-compare_loss = True
+compare_loss = False
 """
 for all comparisons, used these settings which are different than default mlperf settings, in order to enable apples-to-apples comparison with tinygrad math
 - torch.backends.cuda.matmul.allow_tf32 = False
@@ -22,20 +25,70 @@ for all comparisons, used these settings which are different than default mlperf
   - change model.params.unet_config.use_fp16 from true to false
   - comment out this setting (so math is in fp32): lightning.trainer.precision: 16
 
-for clip only:
+for compare_clip only:
 - in clip resblocks: self.mlp[1].approximate="tanh", to match tinygrad gelu fastmath
 """
 
 def md(a, b):
   diff = (a - b).abs()
-  max_diff = diff.max().item()
-  mean_diff = diff.mean().item()
-  ratio = mean_diff / a.abs().mean().item()
-  return mean_diff, ratio, max_diff
+  max_diff = diff.max()
+  mean_diff = diff.mean()
+  ratio = mean_diff / a.abs().mean()
+  return mean_diff.item(), ratio.item(), max_diff.item()
 
 alphas_cumprod = get_alphas_cumprod()
 sqrt_alphas_cumprod = alphas_cumprod.sqrt()
 sqrt_one_minus_alphas_cumprod = (1 - alphas_cumprod).sqrt()
+
+if compare_end_to_end:
+  DATADIR = BASEDIR / "checkpoints"
+
+  ref_losses = safe_load(DATADIR / "eleven_training_steps.safetensors")["loss"]
+  ref_losses.to_("CPU").realize()
+
+  tiny_losses = safe_load(DATADIR / "tiny_losses.safetensors")
+  tiny_losses = Tensor.stack(*[tiny_losses[str(i)].to_("CPU") for i in range(len(tiny_losses))]).unsqueeze(1).realize()
+  mean_diff, ratio, max_diff = md(ref_losses, tiny_losses)
+  #(0.00018372319755144417, 0.00015886307915206042, 0.00032806396484375)
+
+  #### measure mean(abs(diff)) for parameter values
+  tiny_weights = safe_load(DATADIR / "tiny_after_eleven_training_steps.safetensors")
+  #for v in tiny_weights.values(): v.to_("NV")
+
+  tiny_keys = sorted(tiny_weights.keys())
+
+  ref_weights = safe_load(DATADIR / "model_after_eleven_training_steps.safetensors")
+  #for v in ref_weights.values(): v.to_("NV")
+
+  # 866M params * 2 (plus bufs) is too much for 10GB GPU memory
+  #all_tiny = Tensor.cat(*[tiny_weights[k].flatten() for k in tiny_keys])
+  #all_ref = Tensor.cat(*[ref_weights["model.diffusion_model." + k].flatten() for k in tiny_keys])
+  #mean_diff, ratio, max_diff = md(all_ref, all_tiny)
+
+  total_diff_sum = Tensor(0.0)
+  total_a_abs_sum = Tensor(0.0)
+  total_count = 0
+  global_max_diff = Tensor([0.0])
+
+  for k in tiny_keys:
+    a = ref_weights["model.diffusion_model." + k].to_("NV")
+    b = tiny_weights[k].to_("NV")
+    diff = (a - b).abs()
+
+    total_diff_sum   += diff.sum().realize()
+    total_a_abs_sum  += a.abs().sum().realize()
+    total_count      += diff.numel()
+    #global_max_diff = max(global_max_diff, diff.max().item())
+    global_max_diff = global_max_diff.cat(diff.max().unsqueeze(0)).realize()
+
+  global_max_diff = global_max_diff.max().item()
+  mean_diff = (total_diff_sum / total_count).item()
+  ratio     = (total_diff_sum / total_a_abs_sum).item()
+  print("all params diff")
+  print((mean_diff, ratio, global_max_diff))
+  #(1.5695044005767378e-12, 2.3212920474691146e-10, 5.624485321931161e-09)
+  
+  pause = 1
 
 ### sampled latent closeness testing
 if compare_latent:
